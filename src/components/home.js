4eea180061260178a4965fc82510cc8b8d97d454
diame1 <danimahoro75@gmail.com>
2020-05-08 15:46:05 +0200

    add recipe and delete recipe

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,20 @@
 import React, { Component, Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { getAllRecipes } from '../redux/actions/recipe.action';
+import { getAllRecipes, addRecipes, deleteRecipe } from '../redux/actions/recipe.action';
+
 
 export class Recipes extends Component {
     state = {
         currentRecipeIng: [],
         currentRecipeName: '',
-        loading: false
+        currentRecipeDirection: [],
+        currentRecipeId: null,
+        loading: false,
+        name: '',
+        ingredient: '',
+        direction: '',
+        showModal: false
+        
       }
 
     componentWillMount() {
@@ -15,15 +23,31 @@ export class Recipes extends Component {
 
     componentWillReceiveProps({ recipeReducer }) {
         if (recipeReducer.list.length !== 0 ) {
-            this.setState({ currentRecipeIng: recipeReducer.list[0].ingredient, currentRecipeName: recipeReducer.list[0].name})
+            this.setState({ 
+                currentRecipeId: recipeReducer.list[0].id, 
+                currentRecipeIng: recipeReducer.list[0].ingredient, 
+                currentRecipeName: recipeReducer.list[0].name, 
+                currentRecipeDirection: recipeReducer.list[0].direction})
 
         }
-
-        console.log(recipeReducer)
     }
+
+    handleOnChange = (event) => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+      handleSubmit = (e) => {
+        e.preventDefault();
+        const { name, ingredient, direction } = this.state;
+        this.props.addRecipes(name, ingredient, direction);
+      };
    
     render() {
-        const { recipeReducer } = this.props;
+        const { recipeReducer, deleteRecipe } = this.props;
+        const { name, ingredient, direction } = this.state;
         return (
             <div className="container ">
                 <div className="page-header">
@@ -33,8 +57,10 @@ export class Recipes extends Component {
                     { 
                     recipeReducer.list.map( recipe => (
                         <li key={recipe.id} onClick={() => this.setState({
+                            currentRecipeId: recipe.id,
                             currentRecipeIng: recipeReducer.list.filter( item => item.name===recipe.name )[0].ingredient,
-                            currentRecipeName: recipe.name
+                            currentRecipeName: recipe.name,
+                            currentRecipeDirection: recipeReducer.list.filter( item => item.name === recipe.name)[0].direction
                         })
                         }>
                             {recipe.name}</li>
@@ -43,7 +69,7 @@ export class Recipes extends Component {
                 </div>
                 <div className="col-sm-12">
                     <header>
-                    <span>{this.state.currentRecipeName}</span>  <i className="fas fa-trash-alt"></i> <i className="fas fa-edit"></i>
+                    <span>{this.state.currentRecipeName} </span>  <i className="fas fa-trash-alt" onClick={e => deleteRecipe(this.state.currentRecipeId)}></i> <i className="fas fa-edit"></i>
                     </header>
 
                     <div className="col-lg-12 body">
@@ -56,22 +82,62 @@ export class Recipes extends Component {
                             }
                             
                         </ul>
+                        <h5>Direction</h5>
                         <ol>
-                            <li>
-                                In a medium saucepan, melt the butter over moderately low heat. 
-                                Add the garlic and cook for 30 seconds. Stir in the cream, salt, pepper, and artichoke hearts. 
-                                Cook until just heated through, about 3 minutes.
-                            </li>
-                            <li>
-                            In a large pot of boiling, salted water, cook the fusilli until just done, about 13 minutes. 
-                            Drain the pasta and toss with the cream sauce, Parmesan, and chives.
-                            </li>
-                            
-                        </ol>
-
+                        {
+                                this.state.currentRecipeDirection && this.state.currentRecipeDirection.map( ingredient => (
+                                <li key= {ingredient}>{ingredient}</li>
+                                ))
+                            }
+                        </ol>                            
                     </div>
                     <footer>
-                        <i className="far fa-plus-square"></i>
+                        <i className="far fa-plus-square" data-toggle="modal" data-target="#addRecipe"></i>
+                        <div className="modal fade" role="dialog" id="addRecipe">
+                            <div className="modal-dialog"> 
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h3 className="modal-title">Add New Recipe</h3>
+                                        <button type="button" className="close" data-dismiss="modal">&times;</button>
+                                    </div>
+
+                                    <div className="modal-body">
+                                        <form onSubmit={this.handleSubmit}>
+                                            <div className="form-group">
+                                                <input
+                                                    type="text" 
+                                                    name="name" 
+                                                    className="form-control" 
+                                                    placeholder="Recipe Name"
+                                                    onChange={this.handleOnChange}
+                                                    required />
+                                            </div>
+                                            <div className="form-group">
+                                                <textarea
+                                                    name="ingredient" 
+                                                    className="form-control" 
+                                                    placeholder="Add ingredients separate them with -"
+                                                    onChange={this.handleOnChange}
+                                                    required > 
+                                                    </ textarea>
+                                                <textarea 
+                                                    name="direction" 
+                                                    className="form-control mt-3" 
+                                                    placeholder="Add direction separate them with - "
+                                                    onChange={this.handleOnChange}
+                                                    required>
+                                                </textarea> 
+                                            </div>
+                                            <div className="modal-footer">
+                                        <button type="submit" className="btn btn-success">Add</button>
+                                    </div>
+                                        </form>
+                                         
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
                     </footer>
 
                 </div>
@@ -84,6 +150,6 @@ const mapStateToProps = ({ recipeReducer }) => ({
   });
 export default connect(
     mapStateToProps,
-    { getAllRecipes },
+    { getAllRecipes, addRecipes, deleteRecipe },
   )(Recipes)
 
--- src/redux/reducers/recipe.reducer.js
+++ src/redux/reducers/recipe.reducer.js
@@ -1,9 +1,19 @@
-import { GET_RECIPE, GET_RECIPE_ERROR, GET_RECIPE_PENDING} from '../actions/types/action.type';
+import { 
+    GET_RECIPE, 
+    GET_RECIPE_ERROR, 
+    GET_RECIPE_PENDING, 
+    ADD_RECIPE_PENDING, 
+    ADD_RECIPE, 
+    ADD_RECIPE_ERROR, 
+    DELETE_RECIPE, 
+    DELETE_RECIPE_ERROR} 
+from '../actions/types/action.type';
 
 const initialState = {
     list: [],
     error: null,
     loading: false,
+    message:''
   };
 
   export default (state = initialState, { type, payload }) => {
@@ -25,6 +35,35 @@ const initialState = {
                     ...state,
                     error: payload
                 }
+            case ADD_RECIPE_PENDING:
+                return {
+                    ...state,
+                    ...payload,
+                    loading: true
+                };
+            case ADD_RECIPE:
+                return {
+                    ...state,
+                    ...payload,
+                    list: [...state.list, payload],
+                    loading: false
+                }
+            case ADD_RECIPE_ERROR:
+                return {
+                    ...state,
+                    error: payload
+                }
+            case DELETE_RECIPE:
+                return {
+                    ...state,
+                    message: payload.message,
+                    list: state.list.filter(recipe => recipe.id !== payload.id),
+                };
+            case DELETE_RECIPE_ERROR:
+                return {
+                    ...state,
+                    error: payload,
+                };
                 
         default:
             return state;
